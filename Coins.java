0d39af559392a90beb38ee53c56748f5ca80999e
loren.kaso <loren.kaso@post.runi.ac.il>
2023-12-22 11:06:08 +0200

    This is my first ever commit

--- Coins.java
+++ Coins.java
@@ -8,8 +8,8 @@ public class Coins {
             	int coin = input2.nextInt();
             	if(coin < 0){
                 	System.out.println("Error, You need to put a number greater than 0");
-	    	}
-	    	int qurter = coin / 25;
+	    		}
+	    		int qurter = coin / 25;
             	int reminder = coin % 25;
             	System.out.println("Use "+ qurter+ " quarters and " + reminder + " cents");
   	}
--- LinearEq.java
+++ LinearEq.java
@@ -1,3 +1,5 @@
+import java.util.Scanner;
+
 /*
  * Solves linear equations of the form a⋅x + b = c.
  * The program gets a, b, and c as command-line arguments,
@@ -5,5 +7,11 @@
  * Treats the three arguments as well as the computed value as double values
  */
 public class LinearEq {
-	// Put your code here
+    Scanner input3 = new Scanner(System.in);
+    double a = input3.nextDouble();
+    double b = input3.nextDouble();
+    double c = input3.nextDouble();
+    double result = (double) (c - b) / 2;
+    System.out.println( a + " * " + "x" + " + " + b + " = " + c);
+    System.out.print("x" + " = " + result);
 }
